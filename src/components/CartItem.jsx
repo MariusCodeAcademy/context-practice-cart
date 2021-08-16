c9cf1cd577783938c94f428d5657190e2384a032
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-16 10:33:16 +0300

    consume context

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import ItemContext from "../store/ItemContext";
 
 const CartItem = (props) => {
+  const context = useContext(ItemContext);
   return (
     <p className="cart-item">
       Item:
       <strong> {props.item.title}</strong> - price{" "}
       <strong> {props.item.price} eur </strong>
-      <span> Discount: xx %</span>
+      <span> Discount: {context}%</span>
     </p>
   );
 };
